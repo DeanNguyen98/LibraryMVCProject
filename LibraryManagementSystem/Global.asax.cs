4531d06290a4e04f42311260c75f25575dc0afb5
Dean nguyen <101504587+DeanNguyen98@users.noreply.github.com>
2026-05-07 00:40:56 +1000

    Authentication cookies request

--- LibraryManagementSystem/Global.asax.cs
+++ LibraryManagementSystem/Global.asax.cs
@@ -1,10 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
 using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.Security;
 
 namespace LibraryManagementSystem
 {
@@ -16,6 +18,22 @@ namespace LibraryManagementSystem
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_PostAuthenticateRequest(object sender, EventArgs e)
+        {
+            var authCookie = HttpContext.Current.Request.Cookies[FormsAuthentication.FormsCookieName];
+            if (authCookie == null) return;
+
+            var ticket = FormsAuthentication.Decrypt(authCookie.Value);
+            if (ticket == null || ticket.Expired) return;
+
+            var roles = new[] { ticket.UserData };
+            var identity = new FormsIdentity(ticket);
+            var principal = new GenericPrincipal(identity, roles);
+
+            HttpContext.Current.User = principal;
+            System.Threading.Thread.CurrentPrincipal = principal;
         }
     }
 }
