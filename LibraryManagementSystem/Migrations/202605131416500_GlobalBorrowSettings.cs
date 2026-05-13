092ad5726705b83c89a8e52b8587396001b0605c
Dean nguyen <101504587+DeanNguyen98@users.noreply.github.com>
2026-05-14 00:18:52 +1000

    New Migration files

---
+++ LibraryManagementSystem/Migrations/202605131416500_GlobalBorrowSettings.cs
@@ -0,0 +1,24 @@
+﻿namespace LibraryManagementSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class GlobalBorrowSettings : DbMigration
+    {
+        public override void Up()
+        {
+            DropForeignKey("dbo.Libraries", "BorrowSettings_Id", "dbo.BorrowSettings");
+            DropIndex("dbo.Libraries", new[] { "BorrowSettings_Id" });
+            DropColumn("dbo.Libraries", "BorrowSettings_Id");
+            DropColumn("dbo.BorrowSettings", "LibraryId");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.BorrowSettings", "LibraryId", c => c.Int(nullable: false));
+            AddColumn("dbo.Libraries", "BorrowSettings_Id", c => c.Int());
+            CreateIndex("dbo.Libraries", "BorrowSettings_Id");
+            AddForeignKey("dbo.Libraries", "BorrowSettings_Id", "dbo.BorrowSettings", "Id");
+        }
+    }
+}
