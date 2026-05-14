f021d985d53126e94889ca6aa1c76ecd975b759a
20034942-byte <20034942@students.koi.edu.au>
2026-05-15 01:39:48 +1000

    Update project file

---
+++ LibraryManagementSystem/Migrations/202605140632004_AdminBranchChanges.cs
@@ -0,0 +1,24 @@
+﻿namespace LibraryManagementSystem.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AdminBranchChanges : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Books", "BorrowSettingsId", c => c.Int(nullable: false));
+            AddColumn("dbo.BorrowSettings", "LibraryId", c => c.Int(nullable: false));
+            CreateIndex("dbo.Books", "BorrowSettingsId");
+            AddForeignKey("dbo.Books", "BorrowSettingsId", "dbo.BorrowSettings", "Id");
+        }
+        
+        public override void Down()
+        {
+            DropForeignKey("dbo.Books", "BorrowSettingsId", "dbo.BorrowSettings");
+            DropIndex("dbo.Books", new[] { "BorrowSettingsId" });
+            DropColumn("dbo.BorrowSettings", "LibraryId");
+            DropColumn("dbo.Books", "BorrowSettingsId");
+        }
+    }
+}
